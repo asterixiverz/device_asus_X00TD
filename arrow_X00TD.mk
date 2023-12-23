67dd147762118604c5ea7bdaad5d262bf65c646e
Iverz <iverzasterix@gmail.com>
2023-12-23 16:24:49 +0000

    X00TD: Add device maintainer name

--- arrow_X00TD.mk
+++ arrow_X00TD.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# ArrowOS flags
+DEVICE_MAINTAINER := Iverz
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := arrow_X00TD
 PRODUCT_DEVICE := X00TD
